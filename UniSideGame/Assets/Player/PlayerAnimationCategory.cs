542f8609041c93532a20119b8aebf2116d3ad6c2
ganyariya <ganariya2525@gmail.com>
2024-09-01 13:02:20 +0900

    :sparkles: Chapter 4 を実装する

---
+++ UniSideGame/Assets/Player/PlayerAnimationCategory.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationCategory
+{
+    public static string PlayerStop = "PlayerStop";
+    public static string PlayerMove = "PlayerMove";
+    public static string PlayerJump = "PlayerJump";
+    public static string PlayerGoal = "PlayerGoal";
+    public static string PlayerGameOver = "PlayerGameOver";
+}
